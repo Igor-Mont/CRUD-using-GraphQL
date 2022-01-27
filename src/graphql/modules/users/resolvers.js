bba7b27f01644c95a678653b1206364d4abf023c
Igor Mont <igornathandev@gmail.com>
2022-01-27 18:52:43 -0300

    WebSockets (subscriptions)

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,5 @@
 import User from "../../../models/User";
+import { USER_ADDED } from "./channels"
 
 export default {
   User: {
@@ -9,8 +10,21 @@ export default {
     user: async (_, { id }) => await User.findById(id)
   },
   Mutation: {
-    createUser: async (_, { data }) => await User.create(data),
+    createUser: async (_, { data }, { pubsub }) => {
+      const user = await User.create(data);
+
+      pubsub.publish(USER_ADDED, {
+        userAdded: user
+      })
+
+      return user;
+    },
     updateUser: async (_, { id, data }) => await User.findOneAndUpdate(id, data, { new: true }),
     deleteUser: async (_, { id }) => !!(await User.findOneAndRemove(id))
+  },
+  Subscription: {
+    userAdded: {
+      subscribe: (obj, args, { pubsub }) => pubsub.asyncIterator(USER_ADDED)
+    }
   }
 }
--- src/startServer.js
+++ src/startServer.js
@@ -1,10 +1,13 @@
 import { ApolloServer } from "apollo-server";
+import { PubSub } from "graphql-subscriptions";
 import mongoose from "mongoose";
 
 function startServer({ typeDefs, resolvers }) {
   mongoose.connect("mongodb+srv://mongodb_graph:mongo_graph@cluster0.kbzaj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
-  const server = new ApolloServer({ typeDefs, resolvers });
+
+  const pubsub = new PubSub();
+  const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } });
   server.listen().then(({ url }) => console.log(`🔥 Server started at ${url}`));
 }
 
